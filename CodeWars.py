7d3adf2d2cd4078f4a2bd34c61b7677d10eb7d48
mateocabre15-eng <mateocabre15@gmail.com>
2026-05-02 23:10:11 -0300

    Remover archivos subidos por accidente

--- CodeWars.py
+++
@@ -1,7 +0,0 @@
-# ----- Ejercicios de CodeWars: KYU 7 -----
-
-def jumping_number(number):
-    number_str = str(number)
-    for i, letra in enumerate(number_str):
-        print(int(letra), i, letra[0])
-jumping_number(123)
