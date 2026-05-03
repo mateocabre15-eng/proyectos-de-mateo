004a1233acc8a6202e7810c38db568ac654b21b2
mateocabre15-eng <mateocabre15@gmail.com>
2026-05-02 22:53:31 -0300

    Limpiando archivos de prueba y temporales

--- capm basico.py
+++
@@ -1,7 +0,0 @@
-tasaf = float(input("cual es la tasa libre de riesgo?"))
-prima_de_riesgo = float(input("cual es la prima de riesgo?"))
-beta = float(input("cual es la beta?"))
-
-retorno_esperado = tasaf + (prima_de_riesgo * beta)
-
-print(f"el retorno esperado es de {retorno_esperado:.2f}")
--- import requests.py
+++
@@ -1,4 +0,0 @@
-def suma(a, b, c, d):
-    print("si") if a+b>c+d else print("no")
-
-suma(40, 2, 3, 4)
